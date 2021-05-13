ebdb1c31b9bab13745b2fed197c70e32c50dca33
raul <raul.tmachado2@gmail.com>
2021-05-13 00:26:51 -0300

    initial commit

---
+++ src/test/kotlin/br/com/thc/tophat/TophatApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.thc.tophat
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TophatApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
